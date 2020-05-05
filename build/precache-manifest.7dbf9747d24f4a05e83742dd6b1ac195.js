fcb619144ea00e6e993c46e0c790e3a4dcf8b5bc
Ian Chuck-Yin <iancy@github.com>
2020-05-05 00:14:04 -0400

    Build

---
+++ build/precache-manifest.7dbf9747d24f4a05e83742dd6b1ac195.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26ad538c4b31ca640a823163057428f",
+    "url": "/emily/index.html"
+  },
+  {
+    "revision": "11dfc3538b1278ebd20c",
+    "url": "/emily/static/css/main.bc944779.chunk.css"
+  },
+  {
+    "revision": "05c47bca1d4c3f446929",
+    "url": "/emily/static/js/2.7275744c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emily/static/js/2.7275744c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11dfc3538b1278ebd20c",
+    "url": "/emily/static/js/main.e2945de2.chunk.js"
+  },
+  {
+    "revision": "58157c72ee96831fc99a",
+    "url": "/emily/static/js/runtime-main.a0101391.js"
+  },
+  {
+    "revision": "a66a869f15b6343ae9c1bd6aa0a44c76",
+    "url": "/emily/static/media/c_logo.a66a869f.png"
+  },
+  {
+    "revision": "78ba9e222b6ce5cad4dc6e5bf4b304ca",
+    "url": "/emily/static/media/clojure_logo.78ba9e22.png"
+  },
+  {
+    "revision": "7531ac85afcc5c135a07fdb51fc4ce8b",
+    "url": "/emily/static/media/java_logo.7531ac85.png"
+  },
+  {
+    "revision": "1cff25ecb30cb7b4a1e54a0ed53fb22d",
+    "url": "/emily/static/media/js_logo.1cff25ec.png"
+  },
+  {
+    "revision": "29d302681e56304fea530d72910da454",
+    "url": "/emily/static/media/la-icon.29d30268.png"
+  },
+  {
+    "revision": "a7e7f88aa8972942ee1c313519612668",
+    "url": "/emily/static/media/la-image.a7e7f88a.png"
+  },
+  {
+    "revision": "35fdbb2587dfa03c2158650d76c66183",
+    "url": "/emily/static/media/profile_pic_website.35fdbb25.png"
+  },
+  {
+    "revision": "65232491e4595c0c0bea90458771ee14",
+    "url": "/emily/static/media/python_logo.65232491.png"
+  },
+  {
+    "revision": "8d52bff8c49947e02d21204d00cdafd4",
+    "url": "/emily/static/media/slider-bg.8d52bff8.jpg"
+  },
+  {
+    "revision": "50156c71fb2cf47d5cfe134231975922",
+    "url": "/emily/static/media/sql_logo.50156c71.png"
+  },
+  {
+    "revision": "85e2960ff9b5ceccbe3b9816b9a08394",
+    "url": "/emily/static/media/uhn-image.85e2960f.png"
+  },
+  {
+    "revision": "05a1b606f990418c28d4586dd621db54",
+    "url": "/emily/static/media/uhn-logo.05a1b606.png"
+  },
+  {
+    "revision": "12074315ad5ce98a0bfa35cd7aa15597",
+    "url": "/emily/static/media/uncharted-image.12074315.png"
+  },
+  {
+    "revision": "bbbf8d57f06f4041ea2016ba757a8e5a",
+    "url": "/emily/static/media/uncharted-logo.bbbf8d57.png"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/emily/static/media/uwai-image.984874d1.png"
+  },
+  {
+    "revision": "d018832be9add6a10a986b67b9bcd6c2",
+    "url": "/emily/static/media/uwai-logo.d018832b.png"
+  },
+  {
+    "revision": "e3618bb403d53e2ac927ee6246754896",
+    "url": "/emily/static/media/waterloo-image.e3618bb4.png"
+  },
+  {
+    "revision": "cb9abac6570d9e747c4a7be721e68178",
+    "url": "/emily/static/media/waterloo-logo.cb9abac6.png"
+  },
+  {
+    "revision": "a28b4fa7bd2fedcdb8997b2d6f3c159a",
+    "url": "/emily/static/media/youtube_logo.a28b4fa7.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yemilywen.github.io/emily/precache-manifest.6696fceb2b30b9bbd27fed1b452b1292.js"
+  "/emily/precache-manifest.7dbf9747d24f4a05e83742dd6b1ac195.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yemilywen.github.io/emily/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/emily/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
